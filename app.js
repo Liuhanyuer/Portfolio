4913dd129118b08387909463d9691d9ae2fa6e79
Liuhanyuer <hl3409@columbia.edu>
2023-04-26 23:15:59 -0400

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function updateGifPositions(initial) {
 
   gifs.forEach((gif, index) => {
     const scale = Math.pow(0.9, index);
-    const translateX = -25 * index;
+    const translateX = -20 * index;
     gif.style.transform = `scale(${scale}) translateX(${translateX}%)`;
     gif.style.zIndex = 6 - index;
     gif.style.filter = index === 0 ? "grayscale(0%)" : "grayscale(100%)";
@@ -18,15 +18,57 @@ function updateGifPositions(initial) {
 
 document.querySelectorAll(".title").forEach((title, titleIndex) => {
   title.addEventListener("click", () => {
+    const gifContainer = document.querySelector(".gif-container");
+
+    const frontGif = document.querySelector(".gif-container .gif:nth-child(1)");
+
     const gifId = title.dataset.gif;
     const gif = document.getElementById(gifId);
-    const gifContainer = gif.parentElement;
+    gifContainer.insertBefore(gif, frontGif);
 
-    // Move the clicked GIF to the front
-    gifContainer.insertBefore(gif, gifContainer.firstChild);
+    gifContainer.appendChild(frontGif);
 
     updateGifPositions(false);
+    setActiveTitle(titleIndex);
   });
 });
 
 updateGifPositions(true);
+
+function shuffleGifs() {
+  const gifContainer = document.querySelector(".gif-container");
+  const frontGif = document.querySelector(".gif-container .gif:nth-child(1)");
+  gifContainer.appendChild(frontGif);
+  updateGifPositions(false);
+}
+
+function startAutoShuffle(interval) {
+  setInterval(() => {
+    shuffleGifs();
+  }, interval);
+}
+
+startAutoShuffle(3000);
+
+function setActiveTitle(index) {
+  const titles = document.querySelectorAll(".title");
+  titles.forEach((title, i) => {
+    if (i === index) {
+      title.classList.add("title-selected");
+    } else {
+      title.classList.remove("title-selected");
+    }
+  });
+
+  const titleLine = document.querySelector(".title-line");
+  const titleHeight = titles[0].offsetHeight;
+  titleLine.style.transform = `translateY(${index * titleHeight}px)`;
+}
+
+document.querySelectorAll(".title").forEach((title, titleIndex) => {
+  title.addEventListener("click", () => {
+    setActiveTitle(titleIndex);
+  });
+});
+
+setActiveTitle(0);
